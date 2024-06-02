df6ae04598297986db835e9e6b78c430e3734d90
Mahnoor <mahnoort104@gmail.com>
2024-06-02 11:52:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log("WELCOME! to number guessing GAME");
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please! guess a number between 1-10:"
+    }
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! you gusses right number.");
+}
+else {
+    console.log("you guess wrong number!");
+}
